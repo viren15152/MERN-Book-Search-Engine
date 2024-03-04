5b68c2cf1f2af41ee8943a92f98d8e56f5c4b20e
Viren <144032304+viren15152@users.noreply.github.com>
2024-03-04 16:17:49 +0000

    Added the basic boilerplate for GraphQLAPI in my index.js file in my schemas folder

---
+++ server/schemas/index.js
@@ -0,0 +1,5 @@
+// This is my basic boilerplate for a GraphQL API setup in my index.js file
+const typeDefs = require('./typeDefs');
+const resolvers = require('./resolvers');
+
+module.exports = { typeDefs, resolvers };
