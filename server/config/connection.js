4dc052b3864866576efb879f2e976017a8a61223
Viren <144032304+viren15152@users.noreply.github.com>
2024-03-30 05:39:37 +0000

    Added code to connection.js so if the user hasn't setup environment variables

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,7 +1,11 @@
 require("dotenv").config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI)
-console.log("MONGODB_URI:", process.env.MONGODB_URI);
+// Check if MONGODB_URI is defined in environment variables, otherwise use a default URI
+const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/defaultDatabase';
+
+mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('MongoDB connected'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 module.exports = mongoose.connection;
